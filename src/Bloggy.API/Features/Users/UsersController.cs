9b8d3959a7417eebe1b837166ec58440f9563194
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-17 23:34:44 -0400

    add to user

--- src/Bloggy.API/Features/Users/UsersController.cs
+++ src/Bloggy.API/Features/Users/UsersController.cs
@@ -1,6 +1,13 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Conduit.Infrastructure;
+using Conduit.Infrastructure.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Bloggy.API.Features.Users
 {
@@ -8,14 +15,32 @@ namespace Bloggy.API.Features.Users
     public class UsersController
     {
         private readonly IMediator _mediator;
+        private readonly ICurrentUserAccessor _currentUserAccessor;
 
-        public UsersController(IMediator mediator)
+        public UsersController(IMediator mediator, ICurrentUserAccessor currentUserAccessor)
         {
             _mediator = mediator;
+            _currentUserAccessor = currentUserAccessor;
         }
 
         [HttpGet("{username}")]
-        public async Task<IActionResult> GetUser([FromBody] GetUser.Command command)
+        public async Task<IActionResult> GetUser([FromBody] GetUser.Query query)
+        {
+            query.Username = _currentUserAccessor.GetCurrentUsername()
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+        public async Task<IActionResult> GetCurrent()
+        {
+            return await _mediator.Send(command);
+
+        }
+
+        [HttpPut]
+        [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+        public async Task<IActionResult> UpdateUser([FromBody]Edit.Command command)
         {
             return await _mediator.Send(command);
         }
