e9f97cabfaa98474e39f59c0dc1da88e4831082f
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-19 21:17:30 -0400

    tags list query object

--- src/Bloggy.API/Features/Tags/List.cs
+++ src/Bloggy.API/Features/Tags/List.cs
@@ -20,7 +20,7 @@ namespace Bloggy.API.Features.Tags
         public class Model
         {
             public int Id { get; set; }
-            public string Body { get; set; }
+            public string Name { get; set; }
         }
 
         public class Handler : AsyncRequestHandler<Query, Model>
@@ -32,10 +32,11 @@ namespace Bloggy.API.Features.Tags
                 _context = context;
             }
 
-            protected override async Task<Model> HandleCore (Query message, CancellationToken cancellationToken)
+            protected override async Task<Model> HandleCore (Query message)
             {
-                var tags = await _context.Tags.OrderBy(x => x.TagId).AsNoTracking().ToListAsync(cancellationToken);
-                return tags.Select(x => x.TagId).ToList()
+                var tags = await _context.Tags.OrderBy(x => x.Name).AsNoTracking().ToListAsync();
+
+                var model = Mapper.Map<IEnumerable<Model>, IEnumerable<Entities.Tag>> (tags);
 
                 return Result.Ok (model);
             }
